cd3a5690aa08810907e578a547d17840561523ab
TiredGameDev <161785656+TiredGameDev@users.noreply.github.com>
2024-05-06 05:01:44 +0100

    Added Error Handling

--- tirednet.py
+++ tirednet.py
@@ -31,25 +31,34 @@ CONSOLECOLOR_WHITE = "\u001b[37m"
 # print(f"{CONSOLECOLOR_YELLOW}[STARTING] {CONSOLECOLOR_GREEN}Server is starting...{CONSOLECOLOR_RESET}")
 # print(f"{CONSOLECOLOR_YELLOW}[ACTIVE CONNECTIONS] {CONSOLECOLOR_GREEN}69420{CONSOLECOLOR_RESET}")
 
-def init(localhost=True):
+def init(localhost=True, format="utf-8", disconnect="!DISCONNECT"):
     global SERVER, ADDR, FORMAT, DISCONNECT_MESSAGE, PORT, server, msg
     print(f"{CONSOLECOLOR_YELLOW}[INIT] {CONSOLECOLOR_GREEN}Initializing...{CONSOLECOLOR_RESET}")
     PORT = 5050
-    if localhost:
-        SERVER = socket.gethostbyname(socket.gethostname())
-    else:
-        SERVER = socket.gethostbyname(requests.get('https://api.ipify.org').text)
+    try:
+        if localhost:
+            SERVER = socket.gethostbyname(socket.gethostname())
+        else:
+            SERVER = socket.gethostbyname(requests.get('https://api.ipify.org').text)
+    except Exception as e:
+        print(f"{CONSOLECOLOR_RED}[ERROR IN SERVER INIT FUNCTION: FAILED TO SET SERVER] {e}{CONSOLECOLOR_RESET}")
     ADDR = (SERVER, PORT)
-    FORMAT = 'utf-8'
-    DISCONNECT_MESSAGE = "!DISCONNECT"
+    FORMAT = format
+    DISCONNECT_MESSAGE = disconnect
     msg = b''
     print(f"{CONSOLECOLOR_YELLOW}[SERVER CONFIG] {CONSOLECOLOR_GREEN}ADDR: {CONSOLECOLOR_CYAN}{ADDR}{CONSOLECOLOR_RESET}, {CONSOLECOLOR_GREEN}FORMAT: {CONSOLECOLOR_CYAN}{FORMAT}{CONSOLECOLOR_RESET}, {CONSOLECOLOR_GREEN}DISCONNECT: {CONSOLECOLOR_CYAN}{DISCONNECT_MESSAGE}{CONSOLECOLOR_RESET}")
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(ADDR)
+    except Exception as e:
+        print(f"{CONSOLECOLOR_RED}[ERROR IN SERVER INIT FUNCTION: FAILED TO CREATE AND BIND SERVER] {e}{CONSOLECOLOR_RESET}")
 
 def Empty():
-    pass
+    try:
+        pass
+    except Exception as e:
+        print(f"ok now how the fuck did a error happen in a empty function?!?! {e}")
 
 class Client():
     def __init__(self, server, port):
@@ -60,15 +69,24 @@ class Client():
         self.ADDR = (self.SERVER, self.PORT)
         self.connectedCallback = Empty
 
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except Exception as e:
+            print(f"{CONSOLECOLOR_RED}[ERROR IN CLIENT INIT FUNCTION: FAILED TO CREATE AND BIND SERVER] {e}{CONSOLECOLOR_RESET}")
 
     def connect(self):
-        self.client.connect(self.ADDR)
-        self.connectedCallback()
+        try:
+            self.client.connect(self.ADDR)
+            self.connectedCallback()
+        except Exception as e:
+            print(f"{CONSOLECOLOR_RED}[ERROR IN CLIENT CONNECT FUNCTION: FAILED TO CONNECT] {e}{CONSOLECOLOR_RESET}")
     
     def send(self, msg):
-        self.client.send(bytes(msg, self.FORMAT))
-        return self.client.recv(2048).decode(self.FORMAT)
+        try:
+            self.client.send(bytes(msg, self.FORMAT))
+            return self.client.recv(2048).decode(self.FORMAT)
+        except Exception as e:
+            print(f"{CONSOLECOLOR_RED}[ERROR IN CLIENT SEND FUNCTION: FAILED TO SEND] {e}{CONSOLECOLOR_RESET}")
 
 def add_message_callback(message, callback):
     global message_callbacks
@@ -81,7 +99,10 @@ def handle_client(conn, addr):
 
     connected = True
     while connected:
-        msg_ = conn.recv(2048).decode(FORMAT)
+        try:
+            msg_ = conn.recv(2048).decode(FORMAT)
+        except Exception as e:
+            print(f"{CONSOLECOLOR_RED}[ERROR IN HANDLE CLIENT FUNCTION: FAILED TO RECIEVE DATA] {e}{CONSOLECOLOR_RESET}")
         msg_length = len(msg_)
         if msg_length > 0:
             msg = msg_
@@ -95,22 +116,37 @@ def handle_client(conn, addr):
                 print(f"{CONSOLECOLOR_YELLOW}[MSG-{addr}] {CONSOLECOLOR_GREEN}{msg.split('#')[0]} {CONSOLECOLOR_RESET}- {CONSOLECOLOR_CYAN}{msg_data}{CONSOLECOLOR_RESET}")
             
             if msg in message_callbacks:
-                server_wants_response = message_callbacks[msg](addr, msg, conn, msg_data)
-                if server_wants_response:
-                    conn.send(server_wants_response)
-                else:
-                    conn.send(formated("OK", FORMAT))
+                try:
+                    server_wants_response = message_callbacks[msg](addr, msg, conn, msg_data)
+                except Exception as e:
+                    print(f"{CONSOLECOLOR_RED}[ERROR IN HANDLE CLIENT FUNCTION: FAILED TO HANDLE CLIENT MESSAGE CALLBACK] {e}{CONSOLECOLOR_RESET}")
+                try:
+                    if server_wants_response:
+                        conn.send(server_wants_response)
+                    else:
+                        conn.send(formated("OK", FORMAT))
+                except Exception as e:
+                    print(f"{CONSOLECOLOR_RED}[ERROR IN HANDLE CLIENT FUNCTION: FAILED TO SEND RESPONSE DATA] {e}{CONSOLECOLOR_RESET}")
     
     conn.close()
 
 def start():
     global SERVER, ADDR, FORMAT, DISCONNECT_MESSAGE, PORT, server, msg
-    server.listen()
+    try:
+        server.listen()
+    except Exception as e:
+        print(f"{CONSOLECOLOR_RED}[ERROR IN START FUNCTION: FAILED TO START SERVER WITH LISTEN] {e}{CONSOLECOLOR_RESET}")
     print(f"{CONSOLECOLOR_YELLOW}[LISTENING] {CONSOLECOLOR_GREEN}Server is listening on {CONSOLECOLOR_CYAN}{ADDR}{CONSOLECOLOR_RESET}")
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
+        try:
+            conn, addr = server.accept()
+        except Exception as e:
+            print(f"{CONSOLECOLOR_RED}[ERROR IN SERVER LOOP: FAILED TO ACCEPT CONNECTION] {e}{CONSOLECOLOR_RESET}")
+        try:
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+        except Exception as e:
+            print(f"{CONSOLECOLOR_RED}[ERROR IN SERVER LOOP: FAILED TO START THREAD FOR NEW CONNECTION] {e}{CONSOLECOLOR_RESET}")
         if log_connection_ammount:
             print(f"{CONSOLECOLOR_YELLOW}[ACTIVE CONNECTIONS] {CONSOLECOLOR_GREEN}{threading.active_count() - 1}{CONSOLECOLOR_RESET}")
 
